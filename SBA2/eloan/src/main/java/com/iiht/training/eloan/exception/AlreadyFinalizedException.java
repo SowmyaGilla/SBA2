9636f986662633335e6c67f26b5b148aab84e621
wave2wf14 <wave2wf14@wave2wf268.iiht.tech>
2020-12-15 11:18:31 +0530

    Updated code

--- SBA2/eloan/src/main/java/com/iiht/training/eloan/exception/AlreadyFinalizedException.java
+++ SBA2/eloan/src/main/java/com/iiht/training/eloan/exception/AlreadyFinalizedException.java
@@ -2,7 +2,7 @@ package com.iiht.training.eloan.exception;
 
 public class AlreadyFinalizedException extends RuntimeException{
 
-	public AlreadyFinalizedException(String message) {
-		super(message);
+	public AlreadyFinalizedException(String Message) {
+		super(Message);
 	}
 }
